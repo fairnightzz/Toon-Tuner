70472ee404fc41d855ae1d236a11f7b3ff7d6b63
Zhehai Zhang <zhehaizhang1@gmail.com>
2019-03-24 02:54:10 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -23,6 +23,12 @@ class Menu:
                    ,Btn(image.load("Graphics/LightOpen.png"),image.load("Graphics/LightOpen#.png"),\
                            200,300),Btn(image.load("Graphics/LightSettings.png"),image.load("Graphics/LightSettings#.png"),200,500)]
         pos = [[200,100],[200,300],[200,500]]
+
+        editl = ["Import","Main Menu"]
+        editpic = [Btn(image.load("Graphics/LightImport.png"),image.load("Graphics/LightImport#.png"),200,100)\
+                   ,Btn(image.load("Graphics/LightMenu.png"),image.load("Graphics/LightMenu#.png"),\
+                           200,300)]
+        
         #178,100
         
         mx,my = mouse.get_pos()
@@ -40,6 +46,11 @@ class Menu:
                                 print(mainl[n])
                                 mainpic[n].pressed = True
                                 #dialog = self.newFile("Enter File Name")
+
+                    if current == "Edit":
+                        for n in range(len(editpic)):
+                            if Rect(editpic[n].rect[0],editpic[n].rect[1],178,100).collidepoint(mx,my):
+                                editpic[n].pressed = True
                 if evt.type == MOUSEBUTTONUP:
                     if current == "Main":
                         for n in range(len(mainpic)):
@@ -47,8 +58,30 @@ class Menu:
                         for n in range(len(mainpic)):
                             if Rect(mainpic[n].rect[0],mainpic[n].rect[1],178,100).collidepoint(mx,my):
                                 current = mainl[n]
-                                if current == mainl[0]:
+                                if current == mainl[0]:#New file
                                     dialog = self.newFile("Enter File Name")
+                                    current = "Edit"
+                                elif current == mainl[1]:#Open file
+                                    #dialog = self.
+                                    print("in progresss")
+                                    current = "Edit"
+                                elif current == main1[2]: #settings
+                                    print("settings")
+
+                    if current == "Edit":
+                        for n in range(len(editpic)):
+                            editpic[n].pressed = False
+
+                        for n in range(len(editpic)):
+                            if Rect(editpic[n].rect[0],editpic[n].rect[1],178,100).collidepoint(mx,my):
+                                if n == 0:
+                                    print("execute upload pic")
+                                elif n == 1:
+                                    print("Leave")
+
+                                    
+
+                                    
             screen.fill((0,0,0))
             if current == "Main":
                 for i in range(len(mainpic)):
@@ -68,7 +101,11 @@ class Menu:
         message = test.returnmessage()
         download.create_folder("User Files/%s/"%(message))
         return message
-        
+
+    def importPic(self,message):
+        test = takeInput(message)
+        test.mainloop()
+        message = test.returnmessage()
 
 
 class takeInput(Tk):
