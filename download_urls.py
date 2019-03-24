df19ed2e2690995f8b84159198512949b05d215b
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-03-23 20:24:09 -0400

    h

---
+++ download_urls.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib.request
+import os
+
+
+def get_imgs(site):
+    source = requests.get(site).text
+    soup = bs(source, "lxml")
+    urls = [str(url["src"]) for url in soup.findAll("img")
+            if ".jpg" in str(site+url["src"]) or ".png" in str(site+url["src"])]
+    return urls
+
+
+def download(urls):
+    i = 1
+    for url in urls:
+        try:
+            urllib.request.urlretrieve(url, "img{}.png".format(i))
+            i += 1
+        except:
+            pass
+
+
+def create_folder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print("Error")
+
+
+create_folder("./test/")
+download(get_imgs("https://mangadejapan.com/articles/detail/1104"))
+print("done")
